c1d0f6b6bdb0401d75f6ecaf4102904ea2020adf
Mohammad Taha Rahimi <mohammadtahar2007@gmail.com>
2025-11-06 14:39:58 +0330

    adding futures to script.js
    adding functions of toggleSign , sqrt , square.

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ let AC = () => {
 let C = () => {
     result.innerHTML = result.innerHTML.slice(0,-1);
 }
+
+function toggleSign() {
+  if (result.innerHTML.startsWith('-')) {
+    result.innerHTML = result.innerHTML.slice(1);
+  } else if (result.innerHTML !== '') {
+    result.innerHTML = '-' + result.innerHTML;
+  }
+}
+
+function sqrt() {
+  if (result.innerHTML !== '') {
+    let value = eval(result.innerHTML);
+    if (value < 0) {
+      result.innerHTML = "Error";
+    } else {
+      result.innerHTML = Math.sqrt(value);
+    }
+  }
+}
+
+function square() {
+  if (result.innerHTML !== '') {
+    let value = eval(result.innerHTML);
+    result.innerHTML = Math.pow(value, 2);
+  }
+}
